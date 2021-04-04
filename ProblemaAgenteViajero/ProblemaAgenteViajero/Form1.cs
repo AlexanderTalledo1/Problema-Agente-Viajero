7edb83cd0c42addbde9ac5d2b2df98fc663c7c3c
AlexanderTalledo1 <65991289+AlexanderTalledo1@users.noreply.github.com>
2021-04-04 15:12:40 -0500

    Add files via upload

---
+++ ProblemaAgenteViajero/ProblemaAgenteViajero/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace ProblemaAgenteViajero
+{
+    public partial class frmPrincipal : Form
+    {
+        Grafo GetGrafo = new Grafo();
+        ListaGrafos Lista_grafos = new ListaGrafos();
+        //Rectangle Rect = new Rectangle();
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }      
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {            
+        }
+        private void Generar_coordenadas_grafo()
+        {
+            //dibuja las coordenada arriba del grafo
+            foreach(Grafo grafo in Lista_grafos.Obtener_grafos())
+            {
+                Label Coordenadas = new Label
+                {
+                    Visible = true,
+                    Text = "(" + grafo.PosicionX + "," + grafo.PosicionY + ")",                   
+                };
+                Coordenadas.AutoSize.ToString();
+                Coordenadas.Location = new Point(grafo.PosicionX, grafo.PosicionY - 30);
+                GetGrafo.Dibujar_grafo(this.CreateGraphics(), Brushes.Red, this.Size, grafo);
+                Controls.Add(Coordenadas);                
+            }
+        }
+        private void frmPrincipal_MouseClick(object sender, MouseEventArgs e)
+        {
+            Grafo grafo = new Grafo
+            {
+                PosicionX = e.X,
+                PosicionY = e.Y
+            };
+            Lista_grafos.Insertar_grafos(grafo);            
+            Generar_coordenadas_grafo();              
+        }
+
+        private void frmPrincipal_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = "Coordenada x: " + e.X + "Coordenada y: "+ e.Y;
+        }
+    }
+}
---
+++ ProblemaAgenteViajero/ProblemaAgenteViajero/Grafo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProblemaAgenteViajero
+{
+    public class Grafo
+    {
+        public int Numero_grafo { get; set; }
+        public int PosicionX { get; set; }//coordenada X
+        public int PosicionY { get; set; }//coordenada y
+
+        //public int Arista { get; set; }
+
+        public void Dibujar_grafo(Graphics graphics,Brush solid,Size size, Grafo grafo)
+        {
+            if (this.PosicionX < size.Width && this.PosicionY < size.Height)
+            {
+                Random Numeros_aleatorios = new Random();
+                graphics.FillEllipse(solid, grafo.PosicionX, grafo.PosicionY, 40, 40);
+                graphics.Dispose();
+                
+                //this.PosicionX = Numeros_aleatorios.Next(0, size.Width);
+                //this.PosicionY = Numeros_aleatorios.Next(0, size.Height);  
+
+            }
+            
+        }
+    }
+    public class ListaGrafos
+    {
+        public List<Grafo>List_grafos { get; set; }
+        public ListaGrafos()
+        {
+            if(List_grafos==null)
+            {
+                List_grafos = new List<Grafo>();
+            }           
+        }
+        public List<Grafo> Obtener_grafos()
+        {
+            return List_grafos;
+        }
+        public void Insertar_grafos(Grafo grafo)
+        {
+            List_grafos.Add(grafo);
+        }
+        public void Calcular_distancia()
+        {
+           // int distancia = 0;
+            //Adiciona la arista de 2 nodos
+        }
+        public void Conectar_nodos()
+        {
+            foreach(Grafo grafo in List_grafos)
+            {
+               
+            }
+        }
+    }
+}

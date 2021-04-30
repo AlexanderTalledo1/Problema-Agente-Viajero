c6def318536093a0af592b188cfc6bbec5520f38
AlexanderTalledo1 <65991289+AlexanderTalledo1@users.noreply.github.com>
2021-04-30 16:54:49 -0500

    Add files via upload
    Se implementó el mapa del perú

---
+++ Problema-Agente-Viajero/Problema-Agente-Viajero-main/ProblemaAgenteViajero/ProblemaAgenteViajero/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ProblemaAgenteViajero
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnDistancia = new System.Windows.Forms.Button();
+            this.pbxImagen = new System.Windows.Forms.PictureBox();
+            this.listboxGrafos = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(660, 24);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(164, 42);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnDistancia
+            // 
+            this.btnDistancia.Location = new System.Drawing.Point(660, 90);
+            this.btnDistancia.Name = "btnDistancia";
+            this.btnDistancia.Size = new System.Drawing.Size(164, 42);
+            this.btnDistancia.TabIndex = 1;
+            this.btnDistancia.Text = "Distancia";
+            this.btnDistancia.UseVisualStyleBackColor = true;
+            this.btnDistancia.Click += new System.EventHandler(this.btnDistancia_Click);
+            // 
+            // pbxImagen
+            // 
+            this.pbxImagen.Image = global::ProblemaAgenteViajero.Properties.Resources.Mapa_peru;
+            this.pbxImagen.Location = new System.Drawing.Point(12, 3);
+            this.pbxImagen.Name = "pbxImagen";
+            this.pbxImagen.Size = new System.Drawing.Size(616, 687);
+            this.pbxImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxImagen.TabIndex = 2;
+            this.pbxImagen.TabStop = false;
+            this.pbxImagen.Click += new System.EventHandler(this.pbxImagen_Click);
+            this.pbxImagen.Paint += new System.Windows.Forms.PaintEventHandler(this.pbxImagen_Paint);
+            this.pbxImagen.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbxImagen_MouseClick);
+            this.pbxImagen.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbxImagen_MouseMove);
+            // 
+            // listboxGrafos
+            // 
+            this.listboxGrafos.FormattingEnabled = true;
+            this.listboxGrafos.Location = new System.Drawing.Point(650, 244);
+            this.listboxGrafos.Name = "listboxGrafos";
+            this.listboxGrafos.Size = new System.Drawing.Size(189, 446);
+            this.listboxGrafos.TabIndex = 3;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 713);
+            this.Controls.Add(this.listboxGrafos);
+            this.Controls.Add(this.pbxImagen);
+            this.Controls.Add(this.btnDistancia);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "frmPrincipal";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.frmPrincipal_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmPrincipal_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnDistancia;
+        private System.Windows.Forms.PictureBox pbxImagen;
+        private System.Windows.Forms.ListBox listboxGrafos;
+    }
+}
+
---
+++ Problema-Agente-Viajero/Problema-Agente-Viajero-main/ProblemaAgenteViajero/ProblemaAgenteViajero/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace ProblemaAgenteViajero
+{
+    public partial class frmPrincipal : Form
+    {
+        readonly Grafo GetGrafo = new Grafo();
+        readonly ListaGrafos Lista_grafos = new ListaGrafos();
+
+        public string Nombre { get; set; }
+        public int Contador = 0;
+
+
+
+        //Rectangle Rect = new Rectangle();
+        public frmPrincipal()
+        {
+            InitializeComponent();
+
+        }
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+        }
+        private void Generar_coordenadas_grafo()
+        {
+            foreach (Grafo grafo in Lista_grafos.Obtener_grafos())
+            {
+                //Label Coordenadas = new Label//muestra las coordenada arriba del grafo
+                //{
+                //    Visible = true,
+                //    Text = "(" + grafo.PosicionX + "," + grafo.PosicionY + ")",
+                //};
+                //Coordenadas.Size = new Size(80, 15);
+                //Coordenadas.Location = new Point(grafo.PosicionX, grafo.PosicionY - 30);
+
+
+                Label Numero = new Label //muestra el numero que le corresponde al grafo
+                {
+                    Visible = true,
+                    Text = grafo.Numero_grafo.ToString(),
+                };
+                Numero.Size = new Size(20, 14);
+                Numero.BackColor = Color.Red;
+                Numero.Location = new Point(grafo.PosicionX + 13, grafo.PosicionY + 15);
+               
+                pbxImagen.Controls.Add(Numero);
+                GetGrafo.Dibujar_grafo(pbxImagen.CreateGraphics(), Brushes.Red, pbxImagen.Size, grafo);
+            }
+        }
+        private void Generar_camino_grafos()
+        {
+            try
+            {
+                foreach (Grafo grafo in Lista_grafos.Obtener_grafos())
+                {
+                    GraphicsPath camino = new GraphicsPath();
+                    Pen Lapiz = new Pen(Color.Black, 1);
+                    Point[] Puntos = new Point[100];
+                    Puntos[grafo.Numero_grafo] = new Point(grafo.PosicionX + 20, grafo.PosicionY + 20);
+                    camino.AddPolygon(Puntos);
+                    this.CreateGraphics().DrawPath(Lapiz, camino);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+        }
+        private void frmPrincipal_MouseClick(object sender, MouseEventArgs e){}
+
+        private void frmPrincipal_MouseMove(object sender, MouseEventArgs e)
+        {
+             if (Lista_grafos.List_grafos.Count>0)
+             {
+             Lista_grafos.Select_grafo(e.X, e.Y,0);
+             
+             this.Text = "Canidadad de nodos: "+ Lista_grafos.Obtener_grafos().Count+" " +"Coordenada x: " + e.X + " Coordenada y: "+ e.Y+"distancia"+ Lista_grafos.Select_grafo(e.X, e.Y, 0);
+             }
+             else
+             {
+                 this.Text = "Canidadad de nodos: " + Lista_grafos.Obtener_grafos().Count + " " + "Coordenada x: " + e.X + " Coordenada y: " + e.Y;
+             }
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            //Generar_camino_grafos();
+            try
+            {
+                
+                Lista_grafos.Generar_camino(pbxImagen.CreateGraphics());
+                Lista_grafos.Cerrar_camino(pbxImagen.CreateGraphics());
+              
+                 //Lista_grafos.Adicionar_arista(this.CreateGraphics());
+                 //Lista_grafos.Graphs_connected(this.CreateGraphics());
+
+            }
+            catch
+            {
+                MessageBox.Show("Error al generar el camino");
+            }
+           
+        }
+
+        private void btnDistancia_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Lista_grafos.Calcular_distancia().ToString());
+        }
+
+        private void pbxImagen_Paint(object sender, PaintEventArgs e)
+        {
+          
+        }
+
+        private void pbxImagen_MouseClick(object sender, MouseEventArgs e)
+        {
+            Contador++;
+            Grafo grafo = new Grafo
+            {
+                Numero_grafo = Contador,
+                PosicionX = e.X-15,
+                PosicionY = e.Y-15,
+            };
+
+            Lista_grafos.Insertar_grafos(grafo);
+            listboxGrafos.Items.Clear();
+            foreach (Grafo Graph in Lista_grafos.Obtener_grafos())
+            {
+                listboxGrafos.Items.Add("Grafo numero: "+ Graph.Numero_grafo);
+                listboxGrafos.Items.Add("Grafo posicion X: " + Graph.PosicionX);
+                listboxGrafos.Items.Add("Grafo posicion Y: " + Graph.PosicionY);
+                listboxGrafos.Items.Add("_____________________________________");
+               
+            }
+            Generar_coordenadas_grafo();
+        }
+
+        private void pbxImagen_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Lista_grafos.List_grafos.Count > 0)
+            {
+                Lista_grafos.Select_grafo(e.X, e.Y, 0);
+
+                this.Text = "Canidadad de nodos: " + Lista_grafos.Obtener_grafos().Count + " " + "Coordenada x: " + e.X + " Coordenada y: " + e.Y + "distancia" + Lista_grafos.Select_grafo(e.X, e.Y, 0);
+            }
+            else
+            {
+                this.Text = "Canidadad de nodos: " + Lista_grafos.Obtener_grafos().Count + " " + "Coordenada x: " + e.X + " Coordenada y: " + e.Y;
+            }
+        }
+
+        private void pbxImagen_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
